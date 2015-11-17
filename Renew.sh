d8601ae78a7ed7e52f8a75557893db968a6cbf0f
Lewis Goddard <lewisgoddard@eustasy.org>
2015-11-17 19:58:31 +0000

    Delete Renew.sh

--- Renew.sh
+++
@@ -1,3 +0,0 @@
-sudo service nginx stop &&
-~/letsencrypt/letsencrypt-auto certonly --agree-dev-preview --server https://acme-v01.api.letsencrypt.org/directory --standalone -d lewisgoddard.me.uk &&
-sudo service nginx start
