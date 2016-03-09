e1625c3c3c9716903a7a78ab90ae4ba825210bce
Lewis Goddard <lewisgoddard@eustasy.org>
2016-03-09 16:19:10 +0000

    Update renew-ssl.sh

--- renew-ssl.sh
+++ renew-ssl.sh
@@ -2,4 +2,3 @@
 mkdir -p /tmp/letsencrypt-eustasy
 ~/letsencrypt/letsencrypt-auto certonly --config ~/letsencrypt-with-nginx/cli.ini --server https://acme-v01.api.letsencrypt.org/directory -a webroot --webroot-path=/tmp/letsencrypt-eustasy --agree-tos --force-renew "$@"
 sudo service nginx reload
-
