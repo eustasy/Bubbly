5da318fbcce6e90a6afe3bac9c883603b0fb158c
Lewis Goddard <lewisgoddard@eustasy.org>
2016-03-09 16:17:36 +0000

    Move from alias to script.

---
+++ renew-ssl.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir -p /tmp/letsencrypt-eustasy
+~/letsencrypt/letsencrypt-auto certonly --config ~/letsencrypt-with-nginx/cli.ini --server https://acme-v01.api.letsencrypt.org/directory -a webroot --webroot-path=/tmp/letsencrypt-eustasy --agree-tos --force-renew "$@"
+sudo service nginx reload
+
