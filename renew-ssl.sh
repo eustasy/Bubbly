f488ef4d49be6fba98a270c2a15c064610cf1d1f
Lewis Goddard <lewisgoddard@eustasy.org>
2016-10-21 23:54:14 +0100

    Expand options

--- renew-ssl.sh
+++ renew-ssl.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
 mkdir -p /tmp/certbot-with-nginx
-~/certbot/certbot-auto certonly --config ~/certbot-with-nginx/cli.ini --server https://acme-v01.api.letsencrypt.org/directory -a webroot --webroot-path=/tmp/certbot-with-nginx --agree-tos --force-renew "$@"
+
+~/certbot/certbot-auto certonly \
+	--rsa-key-size 4096 \
+	--server https://acme-v01.api.letsencrypt.org/directory \
+	--authenticator webroot \
+	--webroot-path=/tmp/certbot-with-nginx \
+	--agree-tos \
+	--force-renew \
+	"$@"
+
 sudo service nginx reload
