d0e8bb0ae7fa73a2efe44b51730301399b9f641f
Lewis Goddard <lewisgoddard@eustasy.org>
2016-10-22 23:48:02 +0100

    Add warning and option to generate-statics.sh

--- bubbly-generate-statics.sh
+++ bubbly-generate-statics.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+printf "WARNING: The generation of static keys and parameters may take several minutes, and will consume a considerable amount of processing power. If not favourable on a production machine, new keys can be generated elsewhere and copied over a secure connection."
+read -rsp $'Press any key to continue, or Ctrl+C to cancel...\n' -n1 key
 sudo mkdir -p /etc/nginx/ssl &&
 sudo openssl rand 48 -out /etc/nginx/ssl/ticket.key &&
 sudo openssl dhparam -out /etc/nginx/ssl/dhparam4.pem 4096
