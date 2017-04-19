4bd8f503ff3db5adece7fedc00551294ba3259fb
Lewis Goddard <lewisgoddard@eustasy.org>
2017-04-19 16:08:10 +0100

    Add NC 1.4

---
+++ .mdlrc.style.rb
@@ -0,0 +1,9 @@
+all
+rule 'header-style', :style => "atx"
+rule 'line_length', :line_length => 180
+rule 'ol-prefix', :style => "ordered"
+rule 'ul-style', :style => "asterisk"
+
+exclude_rule 'first-header-h1'
+exclude_rule 'first-line-h1'
+exclude_rule 'no-inline-html'
