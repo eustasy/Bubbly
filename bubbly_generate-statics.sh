7aacd561f79f8d77b6fc59bfc2b402dc16c7fada
Lewis Goddard <lewisgoddard@eustasy.org>
2018-05-21 16:27:08 +0100

    space

--- bubbly_generate-statics.sh
+++ bubbly_generate-statics.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-printf "WARNING: The generation of static keys and parameters may take several minutes, and will consume a considerable amount of processing power. If not favourable on a production machine, new keys can be generated elsewhere and copied over a secure connection."
+printf "WARNING: The generation of static keys and parameters may take several minutes, and will consume a considerable amount of processing power. If not favourable on a production machine, new keys can be generated elsewhere and copied over a secure connection. "
 read -rsp $'Press any key to continue, or Ctrl+C to cancel...\n' -n1 key
 sudo mkdir -p /etc/nginx/ssl &&
 sudo openssl rand -out /etc/nginx/ssl/ticket.key 48 &&
