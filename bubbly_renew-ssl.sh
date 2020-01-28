2a01e59c2b666306a73845cb36b95979cd9b14e5
Lewis Goddard <lewisgoddard@eustasy.org>
2020-01-28 11:12:17 +0000

    Update bubbly_renew-ssl.sh

--- bubbly_renew-ssl.sh
+++ bubbly_renew-ssl.sh
@@ -4,7 +4,7 @@ mkdir -p /tmp/bubbly-authenticator
 
 ~/certbot/certbot-auto certonly \
 	--rsa-key-size 4096 \
-	--server https://acme-v01.api.letsencrypt.org/directory \
+	--server https://acme-v02.api.letsencrypt.org/directory \
 	--authenticator webroot \
 	--webroot-path=/tmp/bubbly-authenticator \
 	--agree-tos \
