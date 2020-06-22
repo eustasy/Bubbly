49315cfe93b5afc2ab4a50a6a5bccc6750f36d77
Lewis Goddard <lewisgoddard@eustasy.org>
2020-06-22 23:37:35 +0100

    Update bubbly_generate-statics.sh

--- bubbly_generate-statics.sh
+++ bubbly_generate-statics.sh
@@ -2,5 +2,5 @@
 printf "WARNING: The generation of static keys and parameters may take several minutes, and will consume a considerable amount of processing power. If not favourable on a production machine, new keys can be generated elsewhere and copied over a secure connection. "
 read -rsp $'Press any key to continue, or Ctrl+C to cancel...\n' -n1 key
 sudo mkdir -p /etc/nginx/ssl &&
-sudo openssl rand -out /etc/nginx/ssl/ticket.key 48 &&
-sudo openssl dhparam -out /etc/nginx/ssl/dhparam4.pem 4096
+sudo openssl rand -out /etc/nginx/ssl/ticket.key 80 &&
+sudo openssl dhparam -out /etc/nginx/ssl/dhparam3.pem 3072
