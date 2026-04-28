dac7777b21d8a69ce8ce3f35d56a08d0418c8a21
Lewis Goddard <lewis.goddard@eustasy.org>
2026-04-28 16:58:41 +0100

    Upgrade Mozilla SSL Configs

--- bubbly_renew-ssl.sh
+++ bubbly_renew-ssl.sh
@@ -3,12 +3,12 @@
 sudo mkdir -p /tmp/bubbly-authenticator
 
 sudo certbot certonly \
-	--rsa-key-size 3072 \
+	--key-type ecdsa \
+	--elliptic-curve secp384r1 \
 	--server https://acme-v02.api.letsencrypt.org/directory \
 	--authenticator webroot \
 	--webroot-path=/tmp/bubbly-authenticator \
 	--agree-tos \
-	--force-renew \
 	"$@"
 
 sudo service nginx reload
